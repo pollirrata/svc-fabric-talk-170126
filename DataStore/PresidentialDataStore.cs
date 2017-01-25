efc00176b333a36b1f14e00b0d7fa72a8ab8febd
jorge gaona <jgaona@tiempodevelopment.com>
2017-01-25 16:52:27 -0600

    add missing project

---
+++ DataStore/PresidentialDataStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStore
+{
+    public class PresidentialDataStore
+    {
+        public IDictionary<int,string> GetPresidents()
+        {
+            return new Dictionary<int, string>
+            {
+                {45, "Donald Trump"},
+                {44, "Barack Obama"},
+                {43, "George W Bush"},
+                {42, "Bill Clinton"},
+                {41, "George H Bush"},
+                {40, "Ronald Reagan"},
+
+            };
+        }
+    }
+}
